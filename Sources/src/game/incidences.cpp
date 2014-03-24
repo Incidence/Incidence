625451041f0402c61e9a80305731824bf3c7d4e6
Slvr <slvrgauthier@gmail.com>
2014-03-24 18:27:31 +0100

    gestion_incidences

---
+++ Sources/src/game/incidences.cpp
@@ -0,0 +1,85 @@
+#include "incidences.hpp"
+
+void doIncidences() {
+	
+	//TODO
+	
+}
+
+void erodeFluids(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void dilateFluids(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void expandForests(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void burnForests(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void spawnRessources(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void burnRessources(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void spawnEntities(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void killEntities(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void citizenFeeding(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void citizenSicken(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void citizenBirth(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void citizenJobUpdate(TileMap* tilemap) {
+	
+	//TODO
+	
+}
+
+void bonusPI(TileMap* tilemap) {
+	
+	//TODO
+	
+}
---
+++ Sources/src/game/incidences.hpp
@@ -0,0 +1,27 @@
+#ifndef INCIDENCES_HPP
+#define INCIDENCES_HPP
+
+#include "level/tilemap.hpp"
+
+void doIncidences();
+
+void erodeFluids(TileMap* tilemap);
+void dilateFluids(TileMap* tilemap);
+
+void expandForests(TileMap* tilemap);
+void burnForests(TileMap* tilemap);
+
+void spawnRessources(TileMap* tilemap);
+void burnRessources(TileMap* tilemap);
+
+void spawnEntities(TileMap* tilemap);
+void killEntities(TileMap* tilemap);
+
+void citizenFeeding(TileMap* tilemap);
+void citizenSicken(TileMap* tilemap);
+void citizenBirth(TileMap* tilemap);
+void citizenJobUpdate(TileMap* tilemap);
+
+void bonusPI(TileMap* tilemap);
+
+#endif
